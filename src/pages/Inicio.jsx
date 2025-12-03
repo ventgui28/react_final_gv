dc9a8baab9d9e335a8bbdc609c83cc0ecebb4a81
Guilherme Ventura <guilhermeventura122@gmail.com>
2025-12-03 19:28:01 +0000

    Fix: Use simple Wikimedia URL for Dessert image to fix loading issue

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -43,7 +43,7 @@ const Inicio = () => {
     { 
       nome: 'Sobremesas', 
       valor: 'Dessert', 
-      imagem: 'https://images.unsplash.com/photo-1563729768-74915bd6c276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80' 
+      imagem: 'https://upload.wikimedia.org/wikipedia/commons/0/04/Pound_layer_cake.jpg' 
     },
     { 
       nome: 'Vegetariano', 
